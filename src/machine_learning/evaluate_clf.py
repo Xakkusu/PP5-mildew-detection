4b3e7e0a5329184af42fb618fa4ad5e727c65a1c
Xakkusu <144221908+Xakkusu@users.noreply.github.com>
2025-04-01 16:54:47 +0000

    maint: Add code for the evaluate_clf.py to be able to use evaluation pkl file on detector page

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -1,1 +1,7 @@
 # code for Data Visualization and its functions were adapted and taken from Walkthrough Project 01 Malaria Detector 
+
+from src.data_management import load_pkl_file
+
+
+def load_test_evaluation(version):
+    return load_pkl_file(f"outputs/{version}/evaluation.pkl")
