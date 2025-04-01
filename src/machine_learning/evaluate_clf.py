cbec5251b4cbe861b482f30b25c3ed1170a151d1
Xakkusu <144221908+Xakkusu@users.noreply.github.com>
2025-04-01 16:55:47 +0000

    maint: Import necessary function to load pkl file

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -1,7 +1,8 @@
 # code for Data Visualization and its functions were adapted and taken from Walkthrough Project 01 Malaria Detector 
 
+import streamlit as st
 from src.data_management import load_pkl_file
 
 
 def load_test_evaluation(version):
-    return load_pkl_file(f"outputs/{version}/evaluation.pkl")
+    return load_pkl_file(f'outputs/{version}/evaluation.pkl')
