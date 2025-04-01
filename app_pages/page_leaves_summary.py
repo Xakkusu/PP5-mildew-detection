eb8d7cd997ee3f9e28d115e97e4e66873a4cb301
Xakkusu <144221908+Xakkusu@users.noreply.github.com>
2025-04-01 16:24:11 +0000

    maint: Add code for the page_leaves_summary page

---
+++ app_pages/page_leaves_summary.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+# code for Data Visualization and its functions were adapted and taken from Walkthrough Project 01 Malaria Detector 
+def page_leaves_summary_body():
+
+    st.write("### Project Summary")
+
+    st.info(
+        f"fill this in later")
+
+    st.write(
+        f"fill this in later")
+    
+
+    st.success(
+        f"fill this in later"
+        )
